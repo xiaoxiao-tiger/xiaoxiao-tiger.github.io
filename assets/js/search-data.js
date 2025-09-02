fee30407761cdecc5d6c869b5dd3bf4684d90ca2
xiaoxiao-tiger <37536991+xiaoxiao-tiger@users.noreply.github.com>
2025-09-02 16:15:09 +0000

    Deploying to gh-pages from @ xiaoxiao-tiger/xiaoxiao-tiger.github.io@2240083999d36e9c321732e49baa67885fc88ad6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-我的论文被2025nca年会接受啦-apple",
+          title: '我的论文被2025NCA年会接受啦 :apple:',
+          description: "",
+          section: "News",},{id: "news-我创建了学术公众号论文榨汁机",
+          title: '我创建了学术公众号论文榨汁机',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
